87d85dd474fc4f9fc837dfa8e0d8e61eeeb44726
yongun2 <djkye0001@naver.com>
2024-07-09 18:21:35 +0900

    project init

---
+++ src/main/java/com/beyond/ticketLink/TicketLinkApplication.java
@@ -0,0 +1,13 @@
+package com.beyond.ticketLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beyond/ticketLink/TicketLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyond.ticketLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
