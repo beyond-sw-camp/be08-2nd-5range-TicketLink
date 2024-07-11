3bb29d7bee7cd39c533d2c4e51dbba55aad5584e
yongun2 <djkye0001@naver.com>
2024-07-11 21:34:35 +0900

    Jwt 관련 Dto, VO

---
+++ src/main/java/com/beyond/ticketLink/user/persistence/dto/JwtCreateDto.java
@@ -0,0 +1,14 @@
+package com.beyond.ticketLink.user.persistence.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+public class JwtCreateDto {
+    private final String accessToken;
+    private final String refreshToken;
+    private final String userNo;
+}
