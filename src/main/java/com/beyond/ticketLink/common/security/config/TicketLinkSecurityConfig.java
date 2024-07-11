f21ac5acc9dc6536ff977e0474bfbc97ac2504e0
yongun2 <djkye0001@naver.com>
2024-07-11 23:43:30 +0900

    JWT 인증 설정 추가

--- src/main/java/com/beyond/ticketLink/common/security/config/TicketLinkSecurityConfig.java
+++ src/main/java/com/beyond/ticketLink/common/security/config/TicketLinkSecurityConfig.java
@@ -1,12 +1,19 @@
 package com.beyond.ticketLink.common.security.config;
 
+import com.beyond.ticketLink.common.security.exception.JwtAccessDeniedHandler;
+import com.beyond.ticketLink.common.security.exception.JwtAuthenticationEntryPoint;
+import com.beyond.ticketLink.common.security.filter.JwtAuthenticationFilter;
+import com.beyond.ticketLink.common.security.provider.JwtProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -14,26 +21,44 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 
 @Configuration
+@RequiredArgsConstructor
 public class TicketLinkSecurityConfig {
 
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
     @Bean
-    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain apiFilterChain(HttpSecurity http, JwtProvider jwtProvider) throws Exception {
         http.httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .logout(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
-                .anonymous(AbstractHttpConfigurer::disable);
+                .sessionManagement(session -> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                .anonymous(AbstractHttpConfigurer::disable)
+                .addFilterBefore(
+                        jwtAuthenticationFilter(jwtProvider),
+                        UsernamePasswordAuthenticationFilter.class
+                );
 
         http.authorizeHttpRequests((registry -> {
             registry.requestMatchers(
                     "/api/v1/user/register",
                     "/api/v1/user/check-duplicate",
-                    "/api/v1/mail/**",
                     "/api/v1/user/login",
-                    "/api/v1/user/**"
+                    "/api/v1/mail/**"
             ).permitAll();
 
+            registry.requestMatchers("/api/v1/user/**").hasRole("관리자");
+
+            registry.anyRequest().authenticated();
         }));
+
+        http.exceptionHandling(ex -> {
+            ex.authenticationEntryPoint(jwtAuthenticationEntryPoint);
+            ex.accessDeniedHandler(jwtAccessDeniedHandler);
+        });
 
         return http.build();
     }
@@ -55,5 +80,9 @@ public class TicketLinkSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    JwtAuthenticationFilter jwtAuthenticationFilter(JwtProvider jwtProvider) {
+        return new JwtAuthenticationFilter(jwtProvider);
+    }
 
 }
