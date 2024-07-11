88be97729577839eaf3dc77fd4240509a71e0b15
yongun2 <djkye0001@naver.com>
2024-07-11 21:53:37 +0900

    로그인 요청 컨트롤러 테스트

--- src/test/java/com/beyond/ticketLink/user/ui/controller/UserControllerTest.java
+++ src/test/java/com/beyond/ticketLink/user/ui/controller/UserControllerTest.java
@@ -6,6 +6,7 @@ import com.beyond.ticketLink.smtp.persistence.repository.VerifiedEmailRepository
 import com.beyond.ticketLink.user.application.domain.TicketLinkUserDetails;
 import com.beyond.ticketLink.user.ui.requestbody.CheckDuplicateIdRequest;
 import com.beyond.ticketLink.user.ui.requestbody.UserCreateRequest;
+import com.beyond.ticketLink.user.ui.requestbody.UserLoginRequest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -149,6 +150,77 @@ class UserControllerTest {
                         .value(equalTo(MessageType.ARGUMENT_NOT_VALID.name()))
                 );
 
+    }
+
+    @Test
+    @Transactional
+    void login_shouldLoginRespondWith200() throws Exception {
+        // given
+        String dummyId = "dummyUserA";
+        String dummyPw = "test1234!";
+        UserLoginRequest loginRequest = new UserLoginRequest(dummyId, dummyPw);
+        // when
+        ResultActions perform200 = mvc.perform(post("/api/v1/user/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(loginRequest))
+        );
+        // then
+        perform200.andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.accessToken").exists())
+                .andExpect(jsonPath("$.data.refreshToken").exists());
+    }
+
+    @Test
+    void login_shouldLoginRespondWith404() throws Exception {
+        // given
+        String dummyId = "dummyUserA";
+        String notExistPw = "41412dscssd";
+        UserLoginRequest loginRequest = new UserLoginRequest(dummyId, notExistPw);
+
+        // when
+        ResultActions perform404 = mvc.perform(post("/api/v1/user/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(loginRequest))
+        );
+
+        // then
+        perform404.andExpect(status().isBadRequest());
+        perform404.andExpect(jsonPath("$.errors[0].errorType")
+                .value(equalTo(MessageType.INVALID_PASSWORD.name()))
+        );
+    }
+
+    @Test
+    void login_shouldLoginRespondWith400() throws Exception {
+        // given
+        String dummyId = "dummyUserA";
+        String empty = "";
+        String dummyPw = "test1234!";
+
+        UserLoginRequest loginRequestWithEmptyId = new UserLoginRequest(empty, dummyPw);
+        UserLoginRequest loginRequestWithEmptyPw = new UserLoginRequest(dummyId, empty);
+
+        // when
+        ResultActions perform400WithEmptyId = mvc.perform(post("/api/v1/user/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(loginRequestWithEmptyId))
+        );
+
+        ResultActions perform400WithEmptyPw = mvc.perform(post("/api/v1/user/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(loginRequestWithEmptyPw))
+        );
+
+        // then
+        perform400WithEmptyId.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors[0].errorType")
+                        .value(equalTo(MessageType.ARGUMENT_NOT_VALID.name()))
+                );
+
+        perform400WithEmptyPw.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors[0].errorType")
+                        .value(equalTo(MessageType.ARGUMENT_NOT_VALID.name())
+                        ));
     }
 
     @Test
