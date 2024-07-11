749c3e257b17bbeb3129c5c43693d1907c556073
yongun2 <djkye0001@naver.com>
2024-07-11 14:46:10 +0900

    이메일 인증 기능과 회원가입 기능 연동

--- src/main/java/com/beyond/ticketLink/user/application/service/UserServiceImpl.java
+++ src/main/java/com/beyond/ticketLink/user/application/service/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.beyond.ticketLink.user.application.service;
 
 import com.beyond.ticketLink.common.exception.MessageType;
 import com.beyond.ticketLink.common.exception.TicketLinkException;
+import com.beyond.ticketLink.smtp.persistence.entity.VerifiedEmail;
+import com.beyond.ticketLink.smtp.persistence.repository.VerifiedEmailRepository;
 import com.beyond.ticketLink.user.application.domain.UserRole;
 import com.beyond.ticketLink.user.persistence.dto.UserCreateDto;
 import com.beyond.ticketLink.user.persistence.repository.UserRepository;
@@ -24,14 +26,16 @@ public class UserServiceImpl implements UserService {
 
     private final UserRoleRepository userRoleRepository;
 
-    private final PasswordEncoder passwordEncoder;
+    private final VerifiedEmailRepository verifiedEmailRepository;
 
-    private static final char ENABLE_USER = 'Y';
-    private static final String ROLE_USER = "일반사용자";
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     @Transactional
     public void register(UserCreateRequest request) {
+
+        final char ENABLE_USER = 'Y';
+        final String ROLE_USER = "일반사용자";
 
         // 아이디가 중복 되었을 경우 404_Error throw
         if (userRepository.findUserById(request.id()).isPresent()) {
@@ -42,6 +46,9 @@ public class UserServiceImpl implements UserService {
         UserRole userRole = userRoleRepository.findByRoleName(ROLE_USER)
                 .orElseThrow(() -> new TicketLinkException(MessageType.USER_ROLE_NOT_FOUND));
 
+        // 이메일이 인증되지 않았을 경우 401_Error throw
+        VerifiedEmail verifiedEmail = verifiedEmailRepository.findById(request.email())
+                .orElseThrow(() -> new TicketLinkException(MessageType.EMAIL_UNAUTHORIZED));
 
         // 패스워드 암호화
         String encryptedPassword = encryptPassword(request);
@@ -57,10 +64,13 @@ public class UserServiceImpl implements UserService {
                         .roleNo(userRole.getId())
                         .build()
         );
+
+        // 유저 저장 이후 이메일 인증 완료 정보 삭제
+        verifiedEmailRepository.delete(verifiedEmail);
     }
 
     @Override
-    public void throwErrorWithDuplicateId(String id) {
+    public void checkIdDuplicated(String id) {
 
         if (userRepository.findUserById(id).isPresent()) {
             throw new TicketLinkException(MessageType.DUPLICATE_USER_ID);
