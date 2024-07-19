22d88a6c4222cb4ee7731bf1e5bde30791927632
yongun2 <djkye0001@naver.com>
2024-07-19 23:50:33 +0900

    DTO 생성자를 통한 생성으로 변경

--- src/main/java/com/beyond/ticketLink/user/persistence/dto/JwtCreateDto.java
+++ src/main/java/com/beyond/ticketLink/user/persistence/dto/JwtCreateDto.java
@@ -1,14 +1,16 @@
 package com.beyond.ticketLink.user.persistence.dto;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
-@Builder
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class JwtCreateDto {
-    private final String accessToken;
-    private final String refreshToken;
-    private final String userNo;
+    private String accessToken;
+    private String refreshToken;
+    private String userNo;
 }
