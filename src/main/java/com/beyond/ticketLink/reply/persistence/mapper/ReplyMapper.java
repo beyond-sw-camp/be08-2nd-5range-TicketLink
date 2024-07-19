6527732175e69742f6a80f91c271a952eabf8a66
yongun2 <djkye0001@naver.com>
2024-07-19 22:43:17 +0900

    댓글 CRUD 기능

---
+++ src/main/java/com/beyond/ticketLink/reply/persistence/mapper/ReplyMapper.java
@@ -0,0 +1,19 @@
+package com.beyond.ticketLink.reply.persistence.mapper;
+
+import com.beyond.ticketLink.reply.application.domain.Reply;
+import com.beyond.ticketLink.reply.persistence.dto.ReplyCreateDto;
+import com.beyond.ticketLink.reply.persistence.dto.ReplyUpdateDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Optional;
+
+@Mapper
+public interface ReplyMapper {
+    void insertReply(ReplyCreateDto reply);
+
+    void updateReply(ReplyUpdateDto reply);
+
+    void deleteReply(String replyNo);
+
+    Optional<Reply> selectReplyByReplyNo(String replyNo);
+}
