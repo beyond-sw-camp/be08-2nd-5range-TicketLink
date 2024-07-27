de53605a9682b188ff4fb4ee31e574c5f81a97fe
byHyen <167883838+byHyen@users.noreply.github.com>
2024-07-27 22:03:07 +0900

    Add files via upload

---
+++ notification/persistence/dto/MessageDto.java
@@ -0,0 +1,7 @@
+package com.beyond.ticketLink.notification.persistence.dto;
+
+public class MessageDto {
+    private String resNo;   // 예매번호
+    private String invoiceDate; // 입금기한
+    private String payment; // 결제수단
+}
