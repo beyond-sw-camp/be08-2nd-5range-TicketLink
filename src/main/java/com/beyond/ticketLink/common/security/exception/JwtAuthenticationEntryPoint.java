7a282b80457c6b22390791a4699d2289ec5b136f
yongun2 <djkye0001@naver.com>
2024-07-30 16:20:03 +0900

    인증 실패시 메세지 추가

--- src/main/java/com/beyond/ticketLink/common/security/exception/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/beyond/ticketLink/common/security/exception/JwtAuthenticationEntryPoint.java
@@ -8,11 +8,19 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        PrintWriter writer = response.getWriter();
+        writer.write("{\"error\": \"Unauthorized\", \"message\": \"로그인 후 이용 가능합니다\"}");
+        writer.flush();
+        writer.close();
     }
 }
